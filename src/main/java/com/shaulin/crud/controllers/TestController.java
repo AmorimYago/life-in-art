b02aa7baa733ab9c597660928904e7548b04db9b
Victor Delmondes <v.emanuel2013@gmail.com>
2025-03-12 13:49:35 -0300

    adicionando telas

---
+++ src/main/java/com/shaulin/crud/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.shaulin.crud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String testPage() {
+        return "usuarios";
+    }
+}
